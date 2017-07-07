862af58c41b3789968ccf4ec28635d144fc7fc47
buzai <783243077@qq.com>
2017-07-07 14:18:26 +0800

    change egg-cors , fuck it , destory it

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = app => {
+  // put before other core middlewares
+  app.config.coreMiddlewares.unshift('cors');
+
+  // if security plugin enabled, only allow safe domain support CORS.
+  // app.config.cors.origin = function corsOrigin(ctx) {
+  //   const origin = ctx.get('Origin');
+  //   if (!ctx.isSafeDomain || ctx.isSafeDomain(origin)) {
+  //     return origin;
+  //   }
+  //   return '';
+  // };
+};
